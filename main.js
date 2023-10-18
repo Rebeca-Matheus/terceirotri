b9e4398aa9730d83c722c3a9970c5f32afb942cf
Rebeca-Matheus <127850273+Rebeca-Matheus@users.noreply.github.com>
2023-10-18 09:42:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+while(contador < listaDeTeclas.length) {
+    const instrumento = listaDeTeclas [contador].classList(1);
+    const idAudio = '#som_${instrumento}';
+    console.log(instrumento);
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#som_tecla_splash')};
+    contador = contador + 1;
+}
